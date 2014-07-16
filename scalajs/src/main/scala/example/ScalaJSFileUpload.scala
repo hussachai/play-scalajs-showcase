c5cb24b54d43999a58911b496053803445c726b4
Hussachai Puripunpinyo <hussachai@gmail.com>
2014-07-16 02:30:27 -0700

    second stab on file D&D

--- scalajs/src/main/scala/example/ScalaJSFileUpload.scala
+++ scalajs/src/main/scala/example/ScalaJSFileUpload.scala
@@ -1,29 +1,33 @@
 package example
 
 import org.scalajs.dom
-import org.scalajs.dom.XMLHttpRequest
+import org.scalajs.dom.FileReader
+import shared.Csrf
 import scala.scalajs.js
+import scala.scalajs.js.Any
 import scala.scalajs.js.annotation.JSExport
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
 import js.Dynamic.{global => g, _}
 import scalatags.JsDom._
 import all._
+import org.scalajs.jquery.{jQuery=>$,_}
 
 @JSExport
 object ScalaJSFileUpload {
 
-  def markup = div(
+  def markup(csrfToken: String) = div(
     h1("HTML5 File Drag &amp; Drop API"),
     p("""This is a demonstration of the HTML5 file drag &amp; drop API with asynchronous Ajax file uploads,
       graphical progress bars and progressive enhancement."""),
-    form(id:= "", action:="", target:="", "method".attr:="POST", "enctype".attr:="multipart/form-data"){
+    form(id:= "upload", action:=s"/upload",
+      target:="", "method".attr:="POST", "enctype".attr:="multipart/form-data"){
       fieldset (
         legend("HTML File Upload"),
-        input(`type` := "hidden", id := "maxFileSize", name := "maxFileSize", value := "300000"),
+        input(`type`:="hidden", name:="csrfToken", value:=csrfToken),
         div (
           label(`for` := "", "Files to upload:"),
-          input(`type` := "file", id := "fileSelect", name := "fileSelect[]", "multiple".attr := "multiple"),
-          div(id := "fileDrag")("or drop files here")
+          input(`type` := "file", id := "fileSelect", name := "fileSelect", "multiple".attr := "multiple"),
+          div(id := "fileDrag", backgroundColor:= "green")("or drop files here")
         ),
         div(id := "submitButton") (
           button(`type` := "submit")("Upload Files")
@@ -39,44 +43,147 @@ object ScalaJSFileUpload {
       a(href:="http://sitepoint.com/", "SitePoint.com"), ".")
   )
 
+  trait NodeExt extends dom.Node {
+    var style: js.Dynamic = ???
+    var action: String = ???
+  }
+  trait EventTargetExt extends dom.EventTarget {
+    var files: dom.FileList = ???
+    var className: String = ???
+    var result: js.Any = ???
+
+  }
+  trait EventExt extends dom.Event {
+    var dataTransfer: dom.DataTransfer = ???
+
+    var loaded: Long = ???
+    var total: Long = ???
+  }
+  trait FileReaderExt extends dom.FileReader {
+    var onload: js.Function1[dom.Event, _] = ???
+  }
+
   def scripts = {
+    implicit def monkeyizeNode(e: dom.Node): NodeExt = e.asInstanceOf[NodeExt]
+    implicit def monkeyizeEventTarget(e: dom.EventTarget): EventTargetExt = e.asInstanceOf[EventTargetExt]
+    implicit def monkeyizeEvent(e: dom.Event): EventExt = e.asInstanceOf[EventExt]
+    implicit def monkeyizeFileReader(e: dom.FileReader): FileReaderExt = e.asInstanceOf[FileReaderExt]
 
-    def $(s: String) = dom.document.getElementById(s)
+    val maxFileSize = 3000000l
 
-    def fileSelectHandler(e: dom.Event) = {
-      dom.alert("YES")
+    def $id(s: String) = dom.document.getElementById(s)
+
+    def output(msg: String) = {
+      val m = $id("messages")
+      m.innerHTML = msg + m.innerHTML
     }
 
     def fileDragHover(e: dom.Event) = {
       e.stopPropagation()
       e.preventDefault()
-//      e.target
+      e.target.className = if(e.`type` == "dragover") "hover" else ""
+    }
+
+    def fileSelectHandler(e: dom.Event) = {
+      fileDragHover(e)
+      dom.alert(e.target.files.toString)
+      val files = if(e.target.files.toString != "undefined") e.target.files else {
+        dom.alert(e.dataTransfer.toString)
+        e.dataTransfer.files
+      }
+//      dom.alert((files != null).toString)
+      (0 until files.length).foreach{ i =>
+//        dom.alert(files(i).toString)
+        try {
+          parseFile(files(i))
+          uploadFile(files(i))
+        }catch{
+          case e:Throwable => dom.console.log(e.toString)
+        }
+      }
+    }
+
+    def parseFile(file: dom.File) = {
+      output(
+        s"""
+          |<p>File information: <strong>${file.name}</strong>
+          | type: <strong>${file.`type`}</strong>
+          | size: <strong>${file.size}</strong> bytes</p>
+        """.stripMargin)
+      val reader = new FileReader{}
+      if(file.`type`.indexOf("image") == 0) {
+        reader.onload = (e: dom.Event) => {
+          output(
+            s"""
+              |<p><strong>${file.name}:</strong><br />
+              |<img src=""/>${e.target.result}</p>
+            """.stripMargin)
+        }
+        reader.readAsDataURL(file)
+      }else if(file.`type`.indexOf("text") == 0){
+        reader.onload = (e: dom.Event) => {
+          output(
+            s"""
+              |<p><strong>${file.name}:</strong></p>
+              |<pre>${e.target.result}</pre>
+            """.stripMargin)
+          reader.readAsText(file)
+        }
+      }
+    }
+
+    def uploadFile(file: dom.File) = {
+      val xhr = new dom.XMLHttpRequest
+      if(xhr.upload != null && file.size <= maxFileSize){
+        val o = $id("progress")
+        val progress = o.appendChild(dom.document.createElement("p"))
+        progress.appendChild(dom.document.createTextNode(s"upload ${file.name}"))
+
+        xhr.upload.addEventListener("progress", (e: dom.Event) => {
+          val pc = 100 - (e.loaded / e.total * 100)
+          progress.style.backgroundPosition = s"$pc % 0"
+        }, false)
+
+        xhr.onreadystatechange = (e: dom.Event) => {
+          if(xhr.readyState == 4){
+            progress.className = if(xhr.status == 200) "success" else "failure"
+          }
+        }
+        //start upload
+        xhr.open("POST", $id("upload").action, true)
+        xhr.setRequestHeader("X-Request-With", "XMLHttpRequest")
+        xhr.setRequestHeader("X_FILENAME", file.name)
+        xhr.send(file)
+      }
     }
 
     def init = {
 
-      val submitButton = $("submitButton")
-      $("fileSelect").addEventListener("change", fileSelectHandler _, false)
-      val xhr = new XMLHttpRequest
+      $("#fileDrag").on("dragenter dragstart dragend dragleave dragover drag drop", (e: dom.Event) => {
+        e.preventDefault();
+      });
+
+      $id("fileSelect").addEventListener("change", fileSelectHandler _, false)
+      $id("fileSelect").ondragend
+      val xhr = new dom.XMLHttpRequest
       if(xhr.upload != null){
-        val fileDrag = $("fileDrag")
-//        fileDrag.addEventListener("dragover", )
+        val fileDrag = $id("fileDrag")
+        fileDrag.addEventListener("dragover", fileDragHover _, false)
+        fileDrag.addEventListener("dragleave", fileDragHover _, false)
+        fileDrag.addEventListener("drop", fileSelectHandler _, false)
+        fileDrag.style.display = "block"
+
+        $id("submitButton").style.display = "none"
       }
     }
 
     init
-
-    script {
-      val message = "Hello"
-
-//      dom.alert(message)
-    }
   }
 
   @JSExport
-  def main() = {
+  def main(csrfToken: String) = {
     dom.document.body.innerHTML = ""
-    dom.document.body.appendChild(markup.render)
-    dom.document.body.appendChild(scripts.render)
+    dom.document.body.appendChild(markup(csrfToken).render)
+    scripts
   }
 }
--- scalajvm/app/controllers/FileUploadController.scala
+++ scalajvm/app/controllers/FileUploadController.scala
@@ -1,10 +1,34 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.filters.csrf.CSRFAddToken
+import shared.Csrf
 
 object FileUploadController extends Controller {
 
-  def index = Action {
-    Ok(views.html.fileupload())
+  def index = CSRFAddToken { Action { implicit request =>
+    import play.filters.csrf.CSRF
+    val token = CSRF.getToken(request).map(t=>Csrf(t.value)).getOrElse(Csrf(""))
+    Ok(views.html.fileupload(token))
+  }}
+
+  def upload = Action(parse.multipartFormData) { request =>
+    println("WTF1")
+    println(request.body.files)
+    println("WTF2")
+    request.body.file("fileSelect").map { file =>
+      import java.io.File
+      val filename = file.filename
+      val contentType = file.contentType
+//      picture.ref.moveTo(new File("/tmp/picture"))
+      println(s"filename: $filename")
+      Ok("File uploaded")
+    }.getOrElse {
+//      Redirect(routes.Application.index).flashing(
+//        "error" -> "Missing file"
+//      )
+      BadRequest("Missing file")
+    }
   }
+
 }
