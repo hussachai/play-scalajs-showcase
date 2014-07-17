c944417deedf8f665527835b80e2edae9fbcc984
Hussachai Puripunpinyo <hussachai@gmail.com>
2014-07-17 11:27:33 -0700

    removed unnescessary readAsText

--- scalajs/src/main/scala/example/ScalaJSFileUpload.scala
+++ scalajs/src/main/scala/example/ScalaJSFileUpload.scala
@@ -193,3 +193,129 @@ object ScalaJSFileUpload {
     scripts
   }
 }
+
+/**
+ * Patch for scala-js-dom.
+ */
+object FileReader extends js.Object {
+  //states
+  val EMPTY: Short = 0
+  val LOADING: Short = 1
+  val DONE: Short = 2
+}
+
+class FileReader() extends dom.EventTarget {
+  import dom._
+
+  /**
+   * A DOMError representing the error that occurred while reading the file.
+   *
+   * MDN
+   */
+  val error: DOMError = ???
+
+  /**
+   * A number indicating the state of the FileReader. This will be one of the State constants.
+   * EMPTY   : 0 : No data has been loaded yet.
+   * LOADING : 1 : Data is currently being loaded.
+   * DONE    : 2 : The entire read request has been completed.
+   *
+   * MDN
+   */
+  val readyState: Short = ???
+
+  /**
+   * The file's contents. This property is only valid after the read operation is
+   * complete, and the format of the data depends on which of the methods was used to
+   * initiate the read operation.
+   *
+   * MDN
+   */
+  val result: js.Any = ???
+
+  /**
+   * A handler for the abort event. This event is triggered each time the reading
+   * operation is aborted.
+   *
+   * MDN
+   */
+  var onabort: js.Function1[Event, _] = ???
+
+  /**
+   * A handler for the error event. This event is triggered each time the reading
+   * operation encounter an error.
+   *
+   * MDN
+   */
+  var onerror: js.Function1[Event, _] = ???
+
+  /**
+   * A handler for the load event. This event is triggered each time the reading
+   * operation is successfully completed.
+   *
+   * MDN
+   */
+  var onload: js.Function1[UIEvent, _] = ???
+
+  /**
+   * A handler for the loadstart event. This event is triggered each time the reading
+   * is starting.
+   *
+   * MDN
+   */
+  var onloadstart: js.Function1[ProgressEvent, _] = ???
+
+  /**
+   * A handler for the loadend event. This event is triggered each time the reading
+   * operation is completed (either in success or failure).
+   *
+   * MDN
+   */
+  var onloadend: js.Function1[ProgressEvent, _] = ???
+
+  /**
+   * A handler for the progress event. This event is triggered while reading
+   * a Blob content.
+   *
+   * MDN
+   */
+  var onprogress: js.Function1[ProgressEvent, _] = ???
+
+  /**
+   * Aborts the read operation. Upon return, the readyState will be DONE.
+   *
+   * MDN
+   */
+  def abort(): Unit = ???
+
+  /**
+   * The readAsArrayBuffer method is used to starts reading the contents of the
+   * specified Blob or File. When the read operation is finished, the readyState
+   * becomes DONE, and the loadend is triggered. At that time, the result attribute
+   * contains an ArrayBuffer representing the file's data.
+   *
+   * MDN
+   */
+  def readAsArrayBuffer(blob: Blob): Unit = ???
+
+  /**
+   * The readAsDataURL method is used to starts reading the contents of the specified
+   * Blob or File. When the read operation is finished, the readyState becomes DONE, and
+   * the loadend is triggered. At that time, the result attribute contains a data: URL
+   * representing the file's data as base64 encoded string.
+   *
+   * MDN
+   */
+  def readAsDataURL(blob: Blob): Unit = ???
+
+  /**
+   * The readAsText method is used to read the contents of the specified Blob or File.
+   * When the read operation is complete, the readyState is changed to DONE, the loadend
+   * is triggered, and the result attribute contains the contents of the file as a text string.
+   *
+   * MDN
+   */
+  def readAsText(blob: Blob, encoding: String = "UTF-8"): Unit = ???
+
+}
+
